bf25e6cf784a2516df419707d1b3a4231b2317f6
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-04 12:15:37 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
+import { getApi } from "./redux/contactsOps";
 
 function App() {
+  getApi();
   return (
     <div>
       <h1>Phonebook</h1>
--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,3 +1,31 @@
 import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://6685222db3f57b06dd4b841f.mockapi.io/";
+
+// export const getApi = async () => {
+//   const { data } = await axios.get("contacts");
+//   console.log(data);
+//   return data;
+// };
+
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
+  const { data } = await axios.get("contacts");
+  return data;
+});
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (contact) => {
+    const { data } = await axios.post("contacts", contact);
+    return data;
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id) => {
+    const { data } = await axios.delete(`contacts/:${id}`);
+    return data;
+  }
+);
